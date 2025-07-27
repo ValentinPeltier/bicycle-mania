a3b4f4079156522ccc81817086a5475ba79af1c9
Valentin Peltier <valentin.peltier@proton.me>
2025-07-28 01:20:13 +0200

    ✨ (Entity): add class

---
+++ src/game/Entity.cpp
@@ -0,0 +1,75 @@
+#include "Entity.hpp"
+#include "../core/Logger.hpp"
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+Entity::Entity(const std::string &name) {
+    this->setName(name);
+
+    LOG_INFO("Entity " + this->getFullName() + " created.");
+}
+
+Entity::~Entity() {
+    LOG_INFO("Entity " + this->getFullName() + " deleted.");
+}
+
+const std::string &Entity::getName() const noexcept {
+    return *this->name;
+}
+
+std::string Entity::getFullName() const noexcept {
+    auto parentFullName = this->parent != nullptr ? this->parent->getFullName() : std::string("");
+
+    return parentFullName + "/" + *this->name;
+}
+
+void Entity::setName(const std::string &name) {
+    if (name.contains("/")) {
+        throw std::runtime_error("Name cannot contain slash.");
+    }
+
+    this->name = std::make_unique<std::string>(name);
+}
+
+bool Entity::hasParent() const noexcept {
+    return this->parent != nullptr;
+}
+
+const Entity &Entity::getParent() const {
+    if (!this->hasParent()) {
+        throw std::runtime_error("Entity does not have a parent.");
+    }
+
+    return *this->parent;
+}
+
+void Entity::setParent(const std::shared_ptr<Entity> &parent) {
+    auto previousFullName = this->getFullName();
+
+    this->parent = parent;
+    parent->children.push_back(this->shared_from_this());
+
+    LOG_INFO("Entity " + previousFullName + " is now " + this->getFullName());
+}
+
+bool Entity::hasChildren() const noexcept {
+    return !this->children.empty();
+}
+
+unsigned long Entity::getChildrenCount() const noexcept {
+    return this->children.size();
+}
+
+const std::vector<std::weak_ptr<Entity>> &Entity::getChildren() const noexcept {
+    return this->children;
+}
+
+void Entity::addChild(const std::shared_ptr<Entity> &child) {
+    auto previousFullName = child->getFullName();
+
+    this->children.push_back(child);
+    child->parent = this->shared_from_this();
+
+    LOG_INFO("Entity " + previousFullName + " is now " + child->getFullName());
+}
---
+++ src/game/Entity.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+class Entity : public std::enable_shared_from_this<Entity> {
+    private:
+        std::unique_ptr<std::string> name;
+        std::shared_ptr<Entity> parent = nullptr;
+        std::vector<std::weak_ptr<Entity>> children = {};
+
+    public:
+        Entity(const std::string &name);
+        ~Entity();
+
+        const std::string &getName() const noexcept;
+        std::string getFullName() const noexcept;
+        void setName(const std::string &name);
+
+        bool hasParent() const noexcept;
+        const Entity &getParent() const;
+        void setParent(const std::shared_ptr<Entity> &parent);
+
+        bool hasChildren() const noexcept;
+        unsigned long getChildrenCount() const noexcept;
+        const std::vector<std::weak_ptr<Entity>> &getChildren() const noexcept;
+        void addChild(const std::shared_ptr<Entity> &child);
+};
