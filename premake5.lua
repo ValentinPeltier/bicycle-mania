c3f25538a021cc90cf075b181f79f7fe15e5549e
Valentin Peltier <valentin.peltier@proton.me>
2025-07-29 01:45:57 +0200

    👷: change build system to Premake

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+require ".premake/ecc/ecc"
+
+workspace "GameTest"
+    configurations { "Debug", "Release" }
+    location "build"
+
+project "GameTest"
+    kind "ConsoleApp"
+    language "C++"
+    files { "src/**.hpp", "src/**.cpp" }
+    location "build"
+    includedirs { "include" }
+    cppdialect "C++23"
+
+    filter { "configurations:Debug" }
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter { "configurations:Release" }
+        defines { "NDEBUG" }
+        optimize "On"
