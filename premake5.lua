715d29c8a524b350fee98374070f219050878362
Valentin Peltier <valentin.peltier@proton.me>
2025-07-30 15:01:57 +0200

    👷: update configuration

--- premake5.lua
+++ premake5.lua
@@ -2,20 +2,36 @@ require ".premake/ecc/ecc"
 
 workspace "GameTest"
     configurations { "Debug", "Release" }
-    location "build"
+    architecture "x86_64"
 
 project "GameTest"
     kind "ConsoleApp"
     language "C++"
-    files { "src/**.hpp", "src/**.cpp" }
-    location "build"
-    includedirs { "src" }
     cppdialect "C++23"
+    files { "src/**.hpp", "src/**.cpp" }
+    includedirs { "src" }
 
-    filter { "configurations:Debug" }
+    outputDir = "%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg:lower()}"
+    targetdir ("bin/" .. outputDir)
+    objdir ("obj/" .. outputDir)
+
+    filter "system:windows"
+        systemversion "latest"
+        links {
+            "vulkan-1",
+            "glfw3",
+        }
+
+    filter "system:linux"
+        links {
+            "vulkan",
+            "glfw", "dl", "pthread", "X11", "Xxf86vm", "Xrandr", "Xi",
+        }
+
+    filter "configurations:Debug"
         defines { "DEBUG" }
         symbols "On"
 
-    filter { "configurations:Release" }
-        defines { "NDEBUG" }
+    filter "configurations:Release"
+        defines { "RELEASE" }
         optimize "On"
