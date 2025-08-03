667441c52cf2b9d241edfe9452add3b4fe13651b
Valentin Peltier <valentin.peltier@proton.me>
2025-08-04 00:08:56 +0200

    🚚: rename 'assets' directory to 'resources'

--- src/controllers/Gamepad.cpp
+++ src/controllers/Gamepad.cpp
@@ -70,7 +70,7 @@ void Gamepad::connectionCallback(int id, int event) {
 }
 
 void Gamepad::initialize() {
-    if (glfwUpdateGamepadMappings(File(Path("assets/gamecontrollerdb.txt")).read().c_str()) != GLFW_TRUE) {
+    if (glfwUpdateGamepadMappings(File(Path("resources/gamecontrollerdb.txt")).read().c_str()) != GLFW_TRUE) {
         LOG_ERROR("Failed to update gamepad mappings.");
     }
 
